bb4436a7824b178077d4a9bfb8cb635cf76ffd68
ringol5046 <ringol6@yahoo.com>
2015-08-03 21:12:21 -0400

    Update Letter.java

--- Letter.java
+++ Letter.java
@@ -1,27 +1,27 @@
 /*Provide a class for authoring a simple letter. In the constructor, supply the names of
  the sender and the recipient:
-    public Letter(String from, String to)
+   public Letter(String from, String to)
  Supply a method
-    public void addLine(String line)
+   public void addLine(String line)
  to add a line of text to the body of the letter. Supply a method
-    public String getText()
+   public String getText()
  that returns the entire text of the letter. The text has the form:
-    Dear recipient name :
-    blank line
-    first line of the body
-    second line of the body
-    . . .
-    last line of the body
-    blank line
-    Sincerely,
-    blank line
-    sender name
+   Dear recipient name :
+   blank line
+   first line of the body
+   second line of the body
+   . . .
+   last line of the body
+   blank line
+   Sincerely,
+   blank line
+   sender name
  Also supply a main method that prints this letter.
-    Dear John:
-    I am sorry we must part.
-    I wish you all the best.
-    Sincerely,
-    Mary
+   Dear John:
+   I am sorry we must part.
+   I wish you all the best.
+   Sincerely,
+   Mary
  Construct an object of the Letter class and call addLine twice.*/
 
 public class Letter 
